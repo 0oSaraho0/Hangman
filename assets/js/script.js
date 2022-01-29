a99cf3dce73be6f5a8c868de07c7cdc3233b02c2
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-01-29 16:04:05 +0000

    add code to js to put random word letters into span. fist letter done so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+
 //https://www.worldometers.info/geography/alphabetical-list-of-countries/ infor for countries
-let geography = ['Argentina','Bahamas','Cambodia','Denmark', 'Ethiopia','England','Finland','Guatemala','Haiti','Ireland','Indonesia','Japan','Kazakhstan','Lebanon','Madagascar','Mauritius','Mozambique','Norway','Oman','Paraguay','Portugal','Romania','Senegal','Seychelles','Spain','Scotland','Thailand','Togo','Turkey','Uruguay','Vietnam','Wales','Yemen','Zimbabwe'];
+let geographyArray = ['Argentina','Bahamas','Cambodia','Denmark', 'Ethiopia','England','Finland','Guatemala','Haiti','Ireland','Indonesia','Japan','Kazakhstan','Lebanon','Madagascar','Mauritius','Mozambique','Norway','Oman','Paraguay','Portugal','Romania','Senegal','Seychelles','Spain','Scotland','Thailand','Togo','Turkey','Uruguay','Vietnam','Wales','Yemen','Zimbabwe'];
 //https://www.thoughtco.com/glossary-of-mathematics-definitions-4070804 infor for maths terms
-let maths = ['Abacus','Algorithm','Array','Calculus','Cone','Decagon','Decimal','Diameter','Divide','Exponent','Formula','Fraction','Graph','Hexagon','Integer','Kilometer','Median','Multiply','Numerator','Octagon','Quadrant','Quotient','Radius','Ratio','Rectangle','Rhombus','Subtract','Tangent','Triangle','Variable','Volume','Unit'];
+let mathsArray = ['Abacus','Algorithm','Array','Calculus','Cone','Decagon','Decimal','Diameter','Divide','Exponent','Formula','Fraction','Graph','Hexagon','Integer','Kilometer','Median','Multiply','Numerator','Octagon','Quadrant','Quotient','Radius','Ratio','Rectangle','Rhombus','Subtract','Tangent','Triangle','Variable','Volume','Unit'];
 //https://www.yourschoolgames.com/taking-part/our-sports/  https://www.afpe.org.uk/physical-education/glossary-of-terms/
-let pE = ['Agility','Balance','Cricket','Fitness','Flexibility','Dance','Rounders','Stallball','Shotput','Gymnastics','Archery','Football','Rugby','Hockey','Swimming','Javelin','Running','Aerobics','Muscles','Weights','Treadmill','Lacrosse','Netball','Basketball','Athletics','Tennis','Badminton','Judo','Squash','Vollyball'];
+let pEArray = ['Agility','Balance','Cricket','Fitness','Flexibility','Dance','Rounders','Stallball','Shotput','Gymnastics','Archery','Football','Rugby','Hockey','Swimming','Javelin','Running','Aerobics','Muscles','Weights','Treadmill','Lacrosse','Netball','Basketball','Athletics','Tennis','Badminton','Judo','Squash','Vollyball'];
+let secretWord = "";
+let tries = 7;
+let letter = [];
 
 
 document.addEventListener("DOMContentLoaded",function() {
@@ -12,26 +16,30 @@ document.addEventListener("DOMContentLoaded",function() {
     for (let subject of subjects) {
         subject.addEventListener('click',function() {
             let gameType = this.getAttribute('data-type');
-            runGame(subjectType);
-});
+            subjectType(gameType);
+})
+
 
     }
 
-
-function subjectType () {
+function subjectType (theArray) {
 
     //https://css-tricks.com/snippets/javascript/select-random-item-array/
 
-let randomGeography = geography[Math.floor(Math.random()*geography.length)];
-let randomMaths = maths[Math.floor(Math.random()*maths.length)];
-let randomPE = pE[Math.floor(Math.random()*pE.length)];
+    let randomWord = theArray[Math.floor(Math.random()*theArray.length)];
+// let randomGeography = geographyArray[Math.floor(Math.random()*geographyArray.length)];
+// let randomMaths = mathsArray[Math.floor(Math.random()*mathsArray.length)];
+// let randomPE = pEArray[Math.floor(Math.random()*pEArray.length)]; 
 
-if (subjectType === geography) {
     let word = document.getElementsByClassName('letter');
-    document.word.innerHTML=randomGeography;
 
+    let single = randomWord.length;
+    for (let i = 0; i < single; i++){
+
+    word[0].innerHTML=randomWord[0];
+    }
 };
-};
+
 });
 
 function correctLetter () {
@@ -52,3 +60,8 @@ function playMaths () {
 
 function playPE () {
 }
+
+
+
+
+
