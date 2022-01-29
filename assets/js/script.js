24fb38781eea1ae6f145ad1accfca220e077e8f6
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-01-29 12:51:40 +0000

    add js for selecting random word but it needs more work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,52 @@ let geography = ['Argentina','Bahamas','Cambodia','Denmark', 'Ethiopia','England
 //https://www.thoughtco.com/glossary-of-mathematics-definitions-4070804 infor for maths terms
 let maths = ['Abacus','Algorithm','Array','Calculus','Cone','Decagon','Decimal','Diameter','Divide','Exponent','Formula','Fraction','Graph','Hexagon','Integer','Kilometer','Median','Multiply','Numerator','Octagon','Quadrant','Quotient','Radius','Ratio','Rectangle','Rhombus','Subtract','Tangent','Triangle','Variable','Volume','Unit'];
 //https://www.yourschoolgames.com/taking-part/our-sports/  https://www.afpe.org.uk/physical-education/glossary-of-terms/
-let pe = ['Agility','Balance','Cricket','Fitness','Flexibility','Dance','Rounders','Stallball','Shotput','Gymnastics','Archery','Football','Rugby','Hockey','Swimming','Javelin','Running','Aerobics','Muscles','Weights','Treadmill','Lacrosse','Netball','Basketball','Athletics','Tennis','Badminton','Judo','Squash','Vollyball']
+let pE = ['Agility','Balance','Cricket','Fitness','Flexibility','Dance','Rounders','Stallball','Shotput','Gymnastics','Archery','Football','Rugby','Hockey','Swimming','Javelin','Running','Aerobics','Muscles','Weights','Treadmill','Lacrosse','Netball','Basketball','Athletics','Tennis','Badminton','Judo','Squash','Vollyball'];
+
+
+document.addEventListener("DOMContentLoaded",function() {
+    let subjects = document.getElementsByClassName('subject');
+    
+    for (let subject of subjects) {
+        subject.addEventListener('click',function() {
+            let gameType = this.getAttribute('data-type');
+            runGame(subjectType);
+});
+
+    }
+
+
+function subjectType () {
+
+    //https://css-tricks.com/snippets/javascript/select-random-item-array/
+
+let randomGeography = geography[Math.floor(Math.random()*geography.length)];
+let randomMaths = maths[Math.floor(Math.random()*maths.length)];
+let randomPE = pE[Math.floor(Math.random()*pE.length)];
+
+if (subjectType === geography) {
+    let word = document.getElementsByClassName('letter');
+    document.word.innerHTML=randomGeography;
+
+};
+};
+});
+
+function correctLetter () {
+
+}
+
+function incorrectLetter () {
+
+}
+
+function playGeography () {
+
+}
+
+function playMaths () {
+
+}
+
+function playPE () {
+}
