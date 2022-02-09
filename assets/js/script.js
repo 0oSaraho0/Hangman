42c8f6e480cb676c2b49c6cba2c7243dd5a373cb
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-02-09 18:15:16 +0000

    remove all console.logs and add comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
-//https://www.worldometers.info/geography/alphabetical-list-of-countries/ infor for countries
+// Subject Arrays
 const geographyArray = ['Atlas', 'Bahamas', 'Continent', 'Denmark', 'Ethiopia', 'England', 'Equator', 'Finland', 'Guatemala', 'Globe', 'Haiti', 'Hemisphere', 'Ireland', 'Indonesia', 'Japan', 'Kazakhstan', 'Lebanon', 'Longitude', 'Latitude', 'Madagascar', 'Mozambique', 'Mountain', 'Meridian', 'Map', 'Norway', 'Oman', 'Paraguay', 'Portugal', 'River', 'Romania', 'Senegal', 'Seychelles', 'Scotland', 'Tributary', 'Topography', 'Thailand', 'Togo', 'Turkey', 'Uruguay', 'Vietnam', 'Wales', 'Yemen', 'Zimbabwe'];
-//https://www.thoughtco.com/glossary-of-mathematics-definitions-4070804 infor for maths terms
 const mathsArray = ['Abacus', 'Algorithm', 'Array', 'Calculus', 'Cone', 'Decagon', 'Decimal', 'Diameter', 'Divide', 'Exponent', 'Formula', 'Fraction', 'Graph', 'Hexagon', 'Integer', 'Kilometer', 'Median', 'Multiply', 'Numerator', 'Octagon', 'Quadrant', 'Quotient', 'Radius', 'Ratio', 'Rectangle', 'Rhombus', 'Subtract', 'Tangent', 'Triangle', 'Variable', 'Volume', 'Unit'];
-//https://www.yourschoolgames.com/taking-part/our-sports/  https://www.afpe.org.uk/physical-education/glossary-of-terms/
 const pEArray = ['Agility', 'Balance', 'Cricket', 'Fitness', 'Flexibility', 'Dance', 'Rounders', 'Stallball', 'Shotput', 'Gymnastics', 'Archery', 'Football', 'Rugby', 'Hockey', 'Swimming', 'Javelin', 'Running', 'Aerobics', 'Muscles', 'Weights', 'Treadmill', 'Lacrosse', 'Netball', 'Basketball', 'Athletics', 'Tennis', 'Badminton', 'Judo', 'Squash', 'Vollyball'];
-
+//Global attributes
 const rulesButton = document.getElementById('button-rules');
 const rules = document.getElementsByClassName('rules')[0];
 const choiceButtonsContainer = document.getElementsByClassName('subject-choice-screen')[0];
 const outsideSubjectButton = document.getElementById('subject-choice-button');
+let guessedLetter = document.getElementById('letter-guess');
 let wrongCount = 0;
+let word = [];
+let guessedLetters = [];
+
 /**
  * toggles the hide class on hidden element
  * @param {HTMLElement} hiddenElement 
@@ -17,7 +19,7 @@ let wrongCount = 0;
 function toggleHide(hiddenElement) {
     hiddenElement.classList.toggle('hide');
 }
-
+// loads subject choices when DOM loads.
 document.addEventListener("DOMContentLoaded", function () {
     rulesButton.addEventListener('click', function () {
         toggleHide(rules);
@@ -58,15 +60,11 @@ function pickSubject() {
 pickSubject();
 
 // picks random word from the subject array and inputs it into separate spans in the html as a hidden word.  
-let word = [];
-let guessedLetters =[];
-
 function displayWord(theArray) {
     let randomWord = theArray[Math.floor(Math.random() * theArray.length)].toLowerCase();
     document.getElementById('word').innerHTML = "";
     word = randomWord.split('');
     guessedLetters = randomWord.split('');
-    console.log(word);
 
     for (let i = 0; i < randomWord.length; i++) {
         let newLetter = document.createElement('span');
@@ -76,9 +74,7 @@ function displayWord(theArray) {
     }
 }
 
-let guessedLetter = document.getElementById('letter-guess');
-
-// check letter is a letter and not a number and sends an alert if the wrong value is input.
+// check letter is a letter and not a number.
 function checkLetter() {
     let lettersInputIntoBox = /^[a-zA-Z]/;
     if (guessedLetter.value.match(lettersInputIntoBox)) {
@@ -94,98 +90,58 @@ const usedLetters = [];
 
 // listen to submit event
 form.addEventListener('submit', function (event) {
-    //stop form from reloading the page    
+//stop form from reloading the page    
     event.preventDefault();
-    //get the input letter
+//get the input letter
     const letter = event.target.letterInput.value;
-    console.log(letter);
-    //clear the form  for the next guess
+//clear the form  for the next guess
     form.reset();
-    console.log(word.includes(letter));
-
+//Stops letters that are accidently entered twice to cause a life to be lost.
     if (usedLetters.includes(letter)) {
         return;
     }
+/**adds the entered letters into teh used letters arrah and them adds them to the inner HTML to be seen
+ * during game play */   
     usedLetters.push(letter);
     document.getElementById('used-letters-box').innerHTML += letter;
-    
-
+//checks if the input letter is in the hidden word and removes the class 'hidden-letter' if it is
     for (let i = 0; i < word.length; i++) {
         if (word[i] === letter) {
             document.getElementById('word').children[i].classList.remove('hidden-letter');
-        }    
+        }
     }
+/*draws part of hangman when incorrect letter is entered, adds one to wrongCount and filters the correct
+letters from the word to calculate the win*/
     if (!word.includes(letter)) {
         ++wrongCount;
         drawHangman();
-    }
-    else if (word.includes(letter)) {
+    } else if (word.includes(letter)) {
         guessedLetters = guessedLetters.filter(element => element !== letter);
-        console.log(guessedLetters);
     }
     if (guessedLetters.length === 0) {
         gameWin();
     }
     usedLetters.push(letter);
-
+// Displays the hangman pieces on each wrong entry and runs gameover() when wrongCount = 10
     function drawHangman() {
-        //    let hangman = document.getElementById('hangman');
         let hangmanPieces = document.querySelectorAll('.life');
         for (let j = 0; j < wrongCount; j++) {
-        
             hangmanPieces[j].style.display = 'block';
         }
     }
     if (wrongCount === 10) {
         gameOver();
-        
-
     }
 });
-
+// displays loser screen and disables letter input box when the game is lost.
 function gameOver() {
     let loser = document.getElementById('loser-screen');
     loser.classList.toggle('hide');
     guessedLetter.disabled = 'true';
-    
-
 }
-
-function gameWin(){
+//displays winner screen and disables letter input box when the game is won.
+function gameWin() {
     let winner = document.getElementById('winner-screen');
     winner.classList.toggle('hide');
     guessedLetter.disabled = 'true';
-    
 }
-
-
-
-
-
-
-
-
-
-
-// TODO: Check answer here
-// remove all correctly guessed letter from the word array
-/*   let guessedCorrectly = false;
-    word = word.filter((l, i) => {
-        
-        let hiddenWord = document.getElementById('word');
-        for (let l = 0; l < hiddenWord.length; l++) {
-            if (guessedCorrectly === hiddenWord.length);
-            guessedCorrectly
-        }
-
-        // if its a not correct letter, keep it
-        if (l !== letter) {
-            return l;
-        }
-        else {
-            guessedCorrectly = true;
-        }
-    });
-
-    console.log("The users guess was: " + guessedCorrectly)
-*/
